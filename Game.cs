1c5366e8bec5b31ef211e8a7651b9dceffc4afc2
Lehto1 <harry.lehto13@gmail.com>
2025-10-21 09:47:26 +0200

    LektionArbete

--- Game.cs
+++ Game.cs
@@ -11,7 +11,8 @@ namespace Labb2_ConsolePong
     {
         int width;
         int height;
-
+        Paddle Odd;
+        Paddle Odd2;
         public void StartGame()
         {
             // Setup konsol-fönstret
@@ -19,30 +20,36 @@ namespace Labb2_ConsolePong
             height = Console.WindowHeight;
             Console.CursorVisible = false;
 
-
+            Paddle Odd = new Paddle(1, 2, 4);
+            Paddle Odd2 = new Paddle(4, 6, 9);
         }
 
         public bool Run()
         {
             //Töm hela skärmen i början av varje uppdatering.
             Console.Clear();
+            Odd.Draw();
 
             if (Input.IsPressed(ConsoleKey.UpArrow))
             {
                 //Flytta spelare 1 uppåt
+                Odd.Move(height);
             }
             if (Input.IsPressed(ConsoleKey.DownArrow))
             {
                 //Flytta spelare 1 nedåt
+                Odd.Move(height);
             }
 
             if (Input.IsPressed(ConsoleKey.W))
             {
                 //Flytta spelare 2 uppåt
+                Odd.Move(height);
             }
             if (Input.IsPressed(ConsoleKey.S))
             {
                 //Flytta spelare 2 nedåt
+                Odd.Move(height);
             }
 
 
---
+++ Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    internal class Paddle
+    {
+        int x;
+        int y;
+        int size;
+
+        public Paddle(int y, int x, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+
+        public void Move(int yAmount)
+        {
+            y =+ yAmount;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(y, x);
+            Console.Write("|");
+        }
+    }
+}
